366050ee23e18e014fded099224bccbfb3821298
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-07-01 16:10:51 +0900

    [v0.2.2] Updated Core Engine with additional variables for mods

--- lib/engine/core.rb
+++ lib/engine/core.rb
@@ -38,7 +38,7 @@ module NUSBotgram
     def retrieve_mod(uri, start_year, end_year, sem)
       modules = callback(uri)
       modules_hash = Hash.new
-      module_data = Array.new
+      i = 0
 
       modules.each do |key, value|
         code_query = /[a-zA-Z]{2,3}[\s]?[\d]{4}[a-zA-Z]{0,2}/
@@ -53,19 +53,21 @@ module NUSBotgram
 
         mod_code = json_result["ModuleCode"]
         mod_title = json_result["ModuleTitle"]
+        class_no = json_result["Timetable"][slot]["ClassNo"]
+        lesson_type = json_result["Timetable"][slot]["LessonType"]
         start_time = json_result["Timetable"][slot]["StartTime"]
         end_time = json_result["Timetable"][slot]["EndTime"]
         venue = json_result["Timetable"][slot]["Venue"]
 
-        modules_hash[mod_code] = { :module_title => mod_title,
-                                   :start_time => start_time,
-                                   :end_time => end_time,
-                                   :venue => venue }
-        module_data.push(mod_code)
-        module_data.push(mod_title)
-        module_data.push(start_time)
-        module_data.push(end_time)
-        module_data.push(venue)
+        modules_hash["#{mod_code}-#{i}"] = { :module_code => mod_code,
+                                             :module_title => mod_title,
+                                             :class_no => class_no,
+                                             :lesson_type => lesson_type,
+                                             :start_time => start_time,
+                                             :end_time => end_time,
+                                             :venue => venue }
+
+        i += 1
       end
 
       modules_hash
