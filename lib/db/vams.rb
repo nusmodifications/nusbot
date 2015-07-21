f604ea8d44788dec9b73468307098f2fb7aeafe3
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-07-21 09:52:01 +0900

    [v0.3.42] Created VAMS from MAPNUS
    - Venus Automated Management Script (VAMS) to store MAPNUS geolocation data to DB

---
+++ lib/db/vams.rb
@@ -0,0 +1,32 @@
+require 'redis'
+require 'json'
+
+class VAMS
+  # Configuration
+  CONFIG = YAML.load_file("../lib/config/config.yml")
+
+  location_codes = File.readlines("locations.txt").map(&:strip)
+  location_names = File.readlines("location_names.txt").map(&:strip)
+
+  lat = /\lat\=(\d.\d+)/
+  lon = /\long\=(\d+.\d+)/
+
+  redis = Redis.new(:host => CONFIG[2][:REDIS_HOST], :port => CONFIG[2][:REDIS_PORT], :db => CONFIG[2][:REDIS_DB_MAPNUS])
+
+  location_names = Hash[location_codes.zip(location_names)]
+  locations = File.readlines("loc_db_input.txt").map { |line|
+    location_code, location = line.strip.split(' : ')
+
+    _lat = location.match lat
+    _lon = location.match lon
+    _lat = _lat.to_s.sub!(/\lat\=/, "")
+    _lon = _lon.to_s.sub!(/\long\=/, "")
+
+    results = {
+        title: location_names[location_code],
+        geolocation: [latitude: _lat.to_f, longitude: _lon.to_f]
+    }
+
+    redis.hsetnx("mapnus:locations:#{location_code}", location_code, results.to_json)
+  }
+end
