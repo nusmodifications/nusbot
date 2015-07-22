d51150c721b73adc24d05406bec575f49617afcb
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-07-22 22:49:53 +0900

    [v0.3.53] Processing `brain`
    - Get relative information
    
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

--- lib/engine/brain.rb
+++ lib/engine/brain.rb
@@ -7,14 +7,33 @@ module NUSBotgram
 
     def initialize
       @@core = NUSBotgram::Wit::WitEngine.new(CONFIG[1][:WIT_CLIENT_ACCESS_TOKEN])
+      @@processed_store = ""
     end
 
     public
 
     def pre_process(query)
-      result = @@core.message(query)
+      @@processed_store = @@core.message(query)
+    end
 
-      result
+    def message_id
+      @@processed_store['msg_id']
+    end
+
+    def message_body
+      @@processed_store['msg_body']
+    end
+
+    def intentions
+      @@processed_store['outcomes'][0]['intent']
+    end
+
+    def confidence
+      @@processed_store['outcomes'][0]['confidence']
+    end
+
+    def entities
+      @@processed_store['outcomes'][0]['entities']
     end
   end
 end
--- lib/nusbotgram/version.rb
+++ lib/nusbotgram/version.rb
@@ -1,3 +1,3 @@
 module NUSBotgram
-  VERSION = "0.3.52"
+  VERSION = "0.3.53"
 end
