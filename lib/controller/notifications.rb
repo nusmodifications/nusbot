7d972614437f35b25428fa04c99dea716829e322
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-08-13 21:36:57 +0800

    [v0.3.73] Fix notifications
    - Failed to send notification previously
    - API call using Excon not working
    - Fixed issue
    
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

--- lib/controller/notifications.rb
+++ lib/controller/notifications.rb
@@ -4,15 +4,12 @@ module NUSBotgram
     CONFIG = YAML.load_file("lib/config/config.yml")
 
     def initialize
-      @connection = Excon.new(API_ENDPOINT, :persistent => true)
     end
 
     def send_notify(telegram_id, action, message)
       api_uri = "#{API_ENDPOINT}#{CONFIG[0][:T_BOT_APIKEY_PROD]}#{action}"
       query = { :chat_id => "#{telegram_id}", :text => "#{message}" }
-      # HTTParty.post(api_uri, :query => query)
-      @connection.post(:path => api_uri,
-                       :query => query)
+      HTTParty.post(api_uri, :query => query)
     end
   end
 end
--- lib/nusbotgram/version.rb
+++ lib/nusbotgram/version.rb
@@ -1,3 +1,3 @@
 module NUSBotgram
-  VERSION = "0.3.72"
+  VERSION = "0.3.73"
 end
