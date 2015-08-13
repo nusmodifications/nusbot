b1d763cf86acfb5a26d44ccbee0f526001b188d9
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-08-13 21:52:36 +0800

    [v0.3.75] Updated Notifications Controller
    - Retrieve from Redis
    - Notify users of their scheduled alerts
    
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

--- lib/nusbotgram/version.rb
+++ lib/nusbotgram/version.rb
@@ -1,3 +1,3 @@
 module NUSBotgram
-  VERSION = "0.3.74"
+  VERSION = "0.3.75"
 end
--- notifications_controller.rb
+++ notifications_controller.rb
@@ -1,8 +1,23 @@
+require 'yaml'
 require_relative 'lib/nus_botgram'
 
 module NUSBotgram
   class NotificationsController
-    scheduler = Rufus::Scheduler.new
-    scheduler.join
+    # Configuration & setup
+    CONFIG = YAML.load_file("lib/config/config.yml")
+
+    engine = NUSBotgram::Core.new
+    notify = NUSBotgram::Notifications.new
+    redis_scheduler = Redis.new(:host => CONFIG[3][:SCHEDULER_REDIS_HOST], :port => CONFIG[3][:SCHEDULER_REDIS_PORT], :db => CONFIG[3][:SCHEDULER_REDIS_DB_DEFAULT])
+    scheduler_engine = NUSBotgram::RedisScheduler.new(redis_scheduler, blocking: true)
+
+    scheduler_engine.each do |identifier, at|
+      telegram_id = identifier.split(':').first
+      message_id = identifier.split(':').last
+
+      tasks = engine.get_alert_transactions(telegram_id, message_id)
+      notify.send_notify(telegram_id, Global::SEND_MESSAGE, tasks)
+      engine.remove_alert_transactions(telegram_id, message_id)
+    end
   end
 end
